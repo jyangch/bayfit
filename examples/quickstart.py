3fa72c5302430a79447ae260fc39d3fd79cb843e
Jun Yang <jyang.astro@gmail.com>
2026-06-01 11:30:09 +0800

    test: end-to-end emcee curve fit + quickstart example

---
+++ examples/quickstart.py
@@ -0,0 +1,35 @@
+import os
+import sys
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+import numpy as np
+from curvefit import Data, DataUnit, Infer, Plot
+from curvefit.model.local import ln
+
+savepath = './quickstart'
+
+rng = np.random.default_rng(0)
+x = np.linspace(0, 10, 30)
+yerr = np.full(x.size, 0.5)
+y = 2.0 * x + 1.0 + rng.normal(0, 0.5, x.size)
+
+unit = DataUnit(x, y, yerr=yerr, stat='chi^2')
+data = Data([('d', unit)])
+print(data)
+
+model = ln()
+model.params['logv'].frozen = True
+print(model.par_info.table)
+
+infer = Infer([(data, model)])
+print(infer.par_info.table)
+print(infer.stat_info.table)
+
+post = infer.emcee(nstep=2000, discard=500, resume=False, savepath=savepath)
+print(post)
+
+fig = Plot.infer(post)
+fig.savefig(f'{savepath}/fit.png', dpi=150)
+
+cfig = Plot.post_corner(post)
+cfig.savefig(f'{savepath}/corner.png', dpi=150)
---
+++ tests/test_end_to_end.py
@@ -0,0 +1,34 @@
+import matplotlib
+matplotlib.use('Agg')
+
+import numpy as np
+from curvefit.data.data import Data, DataUnit
+from curvefit.model.local import ln
+from curvefit.infer.infer import Infer
+from curvefit.util.plot import Plot
+
+
+def test_emcee_recovers_linear(tmp_path):
+    rng = np.random.default_rng(0)
+    x = np.linspace(0, 10, 30)
+    ytrue = 2.0 * x + 1.0
+    yerr = np.full(x.size, 0.5)
+    y = ytrue + rng.normal(0, 0.5, x.size)
+
+    unit = DataUnit(x, y, yerr=yerr, stat='chi^2')
+    data = Data([('d', unit)])
+    model = ln()
+    model.params['logv'].frozen = True
+
+    infer = Infer([(data, model)])
+    post = infer.emcee(nstep=400, discard=100, resume=False, savepath=str(tmp_path))
+
+    k = post.par_best_ci[0]
+    b = post.par_best_ci[1]
+    assert abs(k - 2.0) < 0.3
+    assert abs(b - 1.0) < 1.0
+
+    fig = Plot.infer(post, nsample=50, ngrid=50)
+    assert fig is not None
+    cfig = Plot.post_corner(post)
+    assert cfig is not None
