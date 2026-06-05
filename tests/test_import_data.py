2a21a9277636fdac200488ea7ef90cf692aab209
Jun Yang <jyang.astro@gmail.com>
2026-06-05 22:59:16 +0800

    Remove test suite and related ruff config

--- tests/test_import_data.py
+++
@@ -1,5 +0,0 @@
-def test_import_data_module():
-    from bayfit.data import DataUnit as DU
-    from bayfit.data.data import DataUnit
-
-    assert DU is DataUnit
--- tests/test_import_pkg.py
+++
@@ -1,8 +0,0 @@
-def test_import_top_level():
-    from bayfit import BayesInfer, Data, DataUnit, Infer, Plot
-    from bayfit.model.local import line, pl
-
-    assert Data is not None and DataUnit is not None
-    assert Infer is not None and BayesInfer is not None and Plot is not None
-    assert issubclass(BayesInfer, Infer)
-    assert line is not None and pl is not None
