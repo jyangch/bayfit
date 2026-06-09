e23ff63c5149ce07b1bbe9e049dfa5daf95f3ea7
Jun Yang <jyang.astro@gmail.com>
2026-06-09 22:32:37 +0800

    refactor(model): expcut uses Model._asx, drop inline X handling

--- bayfit/model/local/multiplicative.py
+++ bayfit/model/local/multiplicative.py
@@ -9,7 +9,7 @@ from collections import OrderedDict
 
 import numpy as np
 
-from ...util.param import Par
+from ...util.param import Cfg, Par
 from ...util.prior import unif
 from ..model import Multiplicative
 
@@ -19,10 +19,12 @@ class expcut(Multiplicative):
 
     def __init__(self):
         r"""Initialise the exponential cutoff with log-cutoff parameter ``logxc``."""
-        super().__init__()
 
         self.expr = 'expcut'
         self.comment = 'exponential cutoff model'
+
+        self.config = OrderedDict()
+        self.config['threshold'] = Cfg(0.0)
 
         self.params = OrderedDict()
         self.params['logxc'] = Par(0, unif(-10, 10))
@@ -37,8 +39,19 @@ class expcut(Multiplicative):
             1-D array of dimensionless factors in ``(0, 1]`` with the same
             length as ``X``.
         """
-        x = X[:, 0]
 
-        xc = 10 ** self.params['logxc'].value
+        x, scalar = self._asx(X)
 
-        return np.exp(-x / xc)
+        ethr = self.config['threshold'].value
+
+        logxc = self.params['logxc'].value
+        xc = 10**logxc
+
+        nou = np.zeros_like(x, dtype=float)
+
+        i1 = ethr >= x
+        i2 = ethr < x
+        nou[i1] = 1.0
+        nou[i2] = np.exp((ethr - x[i2]) / xc)
+
+        return nou[0] if scalar else nou
